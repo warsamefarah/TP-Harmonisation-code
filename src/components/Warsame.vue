8acdafd8ad98df38804abc44d23ef4a3bc4f59fd
Warsamé <wfarahabdi@gmail.com>
2021-09-22 09:08:10 +0200

    Added personal contribution from warsame-feature

---
+++ src/components/Warsame.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="wrap">
+        <div class="container">
+            <h2>Mais c'est un beau projet de groupe ! 😫👌</h2>
+            <p>Une belle contribution parce que je le vaut bien</p>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Warsame',
+}
+</script>
+
+<style>
+    .wrap {
+        width: 60%;
+        margin: auto;
+    }
+
+    .container {
+        padding: 2rem 0;
+        background-color: teal;
+        color: white;
+        border-radius: 20px;
+    }
+</style>
